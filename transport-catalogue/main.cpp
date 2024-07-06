7b16298e33775fe76cce5c56ce3ecbf1c81ed59c
NEWV0 <152032646+NEWV0@users.noreply.github.com>
2024-07-06 20:13:54 +0300

    sprint 8 update 2

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,11 +1,12 @@
 #include "transport_catalogue.h"
 #include "input_reader.h"
 #include "stat_reader.h"
+#include <iostream>
 
 using namespace std;
 
 int main() {
     TransportCatalogue catalogue;
-    input_(catalogue); 
-    output_(catalogue);
+    input_(catalogue, cin);
+    output_(catalogue, cin, cout);
 }
